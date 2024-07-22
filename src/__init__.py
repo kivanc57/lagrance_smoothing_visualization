e9fed85a3a4bc781647f27725132be4967d4baa1
Kivanc Gordu <kivancgordu@hotmail.com>
2024-07-22 12:59:08 +0000

    Add log and metadata info to __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,8 +2,23 @@ from src.generate_map import get_map
 from src.lagrance_smoothing import set_lagrance
 from src.visualization import make_image
 from config.constants import SIDE_LENGTH, MISSING_VALUES, TOLERANCE, STEP_LIMIT_MULTIPLIER
+import logging
 
-PACKAGE_VERSION = '1.0.0'
+logging.basicConfig(
+  level=logging.INFO,
+  filename='src.log',
+  filemode='a',
+  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+logger.info("Package is initialized")
+
+
+__version__ = "1.0.0"
+__date__ = "22-06-2024"
+__email__ = "kivancgordu@hotmail.com"
+__status__ = "production"
 
 __all__ = [
   'get_map', 'set_lagrance', 'make_image',
